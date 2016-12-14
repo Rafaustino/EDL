1f2534a75f7c01953cdf81c657d91f7997f51239
Rafael Faustino <rafaelsilvamengo@gmail.com>
2016-12-14 18:05:05 -0200

    Update trabalho8.elm

--- trabalhos/trabalho8.elm
+++ trabalhos/trabalho8.elm
@@ -1,37 +1,111 @@
 import Html exposing (text)
 
 type alias Env = (String -> Int)
+
+-- Zero
 zero : Env
 zero = \ask -> 0
 
-type Exp = Add Exp Exp
+
+
+-- Operacoes
+
+type Exp = Consta String
          | Num Int
          | Var String
          
+         -- Matematicas
+         | Add Exp Exp
+         | Sub Exp Exp
+         | Div Exp Exp
+         | Mul Exp Exp
+         | Modu Exp Exp
+         | El Exp Exp
+         
+         --logicas
+         | Ma Exp Exp
+         | Me Exp Exp
+         | Igual Exp Exp
+         
+
+         
 type Prog = Attr String Exp
-          | Seq Prog Prog         
-          
+          | If Exp Prog Prog
+          | While Exp Prog
+          | DefConst String Exp
+          | Seq Prog Prog   
+ 
+ 
+ 
 evalExp : Exp -> Env -> Int
 evalExp exp env =
     case exp of
-        Add exp1 exp2  -> (evalExp exp1 env) + (evalExp exp2 env)
         
-        Num v          -> v
-        Var var        -> (env var)
+        Num x               -> x
+        Var variavel            -> (env variavel)
+        Consta constante        -> (env constante)
         
+        -- Operacpes matematicas
+        
+        Add exp1 exp2       -> (evalExp exp1 env) + (evalExp exp2 env)
+        Sub exp1 exp2       -> (evalExp exp1 env) - (evalExp exp2 env)        
+        Mul exp1 exp2     -> (evalExp exp1 env) * (evalExp exp2 env)
+        Div exp1 exp2       -> (evalExp exp1 env) // (evalExp exp2 env)
+        Modu exp1 exp2       -> (evalExp exp1 env) % (evalExp exp2 env)
+        El x e   -> (evalExp x env) ^ (evalExp e env)
+        
+        --Operacoes logicas
+        
+        Ma exp1 exp2        ->
+                if (evalExp exp1 env) > (evalExp exp2 env) then
+                    1
+                else
+                    0
+        Me exp1 exp2        ->
+                if (evalExp exp1 env) < (evalExp exp2 env) then
+                    1
+                else
+                    0
+        Igual exp1 exp2        ->
+                if (evalExp exp1 env) == (evalExp exp2 env) then
+                    1
+                else
+                    0
+         
+ 
+ 
 evalProg : Prog -> Env -> Env
 evalProg s env =
     case s of
-        Seq s1 s2 ->
+        Seq s1 s2       ->
             (evalProg s2 (evalProg s1 env))
-        Attr var exp ->
+        Attr variavel exp    ->
+            let
+                valor = (evalExp exp env)
+            in
+                \ask -> if ask == variavel then valor else (env ask)        
+        If cond ex1 ex2   ->
+                if (evalExp cond env) /= 0 then
+                    (evalProg ex1 env)
+                else
+                    (evalProg ex2 env)
+        While condicao c   ->
+                if (evalExp condicao env) == 0 then
+                    env
+                else
+                    (evalProg (Seq c s) env)
+                    
+        DefConst constante exp ->
             let
                 val = (evalExp exp env)
             in
-                \ask -> if ask==var then val else (env ask)
+                \ask -> if ask == constante then
+                                val
+                            else
+                                (env ask)
 ----------------------------------------------------------------------------------------------------------------
 lang : Prog -> Int
-lang pz = ((evalProg pz zero) "ret")
+lang p = ((evalProg p zero) "ret")
 
 p1 : Prog
 p1 = (Attr "ret" (Add (Num 21) (Num 9)))
@@ -39,131 +113,15 @@ p1 = (Attr "ret" (Add (Num 21) (Num 9)))
 p2 : Prog
 p2 = Seq
         (Attr "x"   (Num 15))
-        (Attr "ret" (Add (Var "x") (Num 9)))
-        
-              
--------------------------------------------------------------------------------------------------------------         
-type Exp2 = Sub Exp Exp
-            | Num2 Int
-            | Var2 String
-            
-type Prog2 = Attr2 String Exp2
-          | Seq2 Prog2 Prog2            
-          
-evalExp2 : Exp2 -> Env -> Int
-evalExp2 exp2 env = 
-      case exp2 of
-        Sub exp3 exp4   -> (evalExp exp3 env) - (evalExp exp4 env)
-        
-        Num2 v2          -> v2
-        Var2 var2        -> (env var2)
-        
-        
-evalProg2 : Prog2 -> Env -> Env
-evalProg2 s env =
-    case s of
-        Seq2 s3 s4 ->
-            (evalProg2 s4 (evalProg2 s3 env))
-        Attr2 var2 exp2 ->
-            let
-                val = (evalExp2 exp2 env)
-            in
-                \ask -> if ask==var2 then val else (env ask)
------------------------------------------------------------------------------------------------------------------                
-lang2 : Prog2 -> Int
-lang2 sz = ((evalProg2 sz zero) "ret")
+        (Attr "ret" (Sub (Var "x") (Num 9)))
+-- Maior        
+p4 : Prog
+p4 = (If (Ma (Num 15) (Num 10)) (Attr "ret" (Num 1)) ((Attr "ret" (Num 0))))
+-- Menor
+p5 : Prog
+p5 = (If (Me (Num 15) (Num 10)) (Attr "ret" (Num 1)) ((Attr "ret" (Num 0))))
 
-s1 : Prog2
-s1 = (Attr2 "ret" (Sub (Num 11) (Num 9)))
 
-s2 : Prog2
-s2 = Seq2
-        (Attr2 "x"   (Num2 10))
-        (Attr2 "ret" (Sub (Var "x") (Num 9)))
-  
------------------------------------------------------------------------------------------------------------------            
-type Exp3 = Mul Exp Exp
-            | Num3 Int
-            | Var3 String
-            
-type Prog3 = Attr3 String Exp3
-          | Seq3 Prog3 Prog3            
-          
-evalExp3 : Exp3 -> Env -> Int
-evalExp3 exp3 env = 
-      case exp3 of
-        Mul exp5 exp6   -> (evalExp exp5 env) * (evalExp exp6 env)
-        
-        Num3 v3          -> v3
-        Var3 var3        -> (env var3)
-
-evalProg3: Prog3 -> Env -> Env
-evalProg3  s env =
-    case s of
-        Seq3 s5 s6 ->
-            (evalProg3 s6 (evalProg3 s5 env))
-        Attr3 var3 exp3 ->
-            let
-                val = (evalExp3 exp3 env)
-            in
-                \ask -> if ask==var3 then val else (env ask)
-------------------------------------------------------------------------------------------------------------------
-lang3 : Prog3 -> Int
-lang3 vz = ((evalProg3 vz zero) "ret")
-
-v1 : Prog3
-v1 = (Attr3 "ret" (Mul (Num 11) (Num 9)))
-
-v2 : Prog3
-v2 = Seq3
-        (Attr3 "x"   (Num3 10))
-        (Attr3 "ret" (Mul (Var "x") (Num 9)))
-
-------------------------------------------------------------------------------------------------------------------
-type Exp4 = Div Exp Exp
-            | Num4 Int
-            | Var4 String
-
-type Prog4 = Attr4 String Exp4
-          | Seq4 Prog4 Prog4
-      
-evalExp4 : Exp4 -> Env -> Int
-evalExp4 exp4 env = 
-      case exp4 of
-        Div exp7 exp8   -> (evalExp exp7 env) // (evalExp exp8 env)
-        
-        Num4 v4          -> v4
-        Var4 var4       -> (env var4)
-
-evalProg4: Prog4 -> Env -> Env
-evalProg4  s env =
-    case s of
-        Seq4 s7 s8 ->
-            (evalProg4 s8 (evalProg4 s7 env))
-        Attr4 var4 exp4 ->
-            let
-                val = (evalExp4 exp4 env)
-            in
-                \ask -> if ask==var4 then val else (env ask)
---------------------------------------------------------------------------------------------------------------------------------- 
-lang4 : Prog4 -> Int
-lang4 dz = ((evalProg4 dz zero) "ret")
-
-d1 : Prog4
-d1 = (Attr4 "ret" (Div (Num 20) (Num 5)))
-
-d2 : Prog4
-d2 = Seq4
-        (Attr4 "x"   (Num4 10))
-        (Attr4 "ret" (Div (Var "x") (Num 2)))
- 
 ----------------------------------------------------------------------------------------------------------------------------------
 
---main = text (toString (lang p1))
---main = text (toString (lang p2))
---main = text (toString (lang2 s1))
---main = text (toString (lang2 s2))
---main = text (toString (lang3 v1))
---main = text (toString (lang3 v2))
---main = text (toString (lang4 d1))
-main = text (toString (lang4 d2))
+main = text (toString (lang p1))
